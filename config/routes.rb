e22bdc181a1e6ac6991e58cd2e79f9e08e73a5af
Liv Cella <livncella@gmail.com>
2019-04-10 20:40:19 -0600

    added functionality for activeadmin. WORKS ONLY IN CHROMIUM BASED BROWSERS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   mount_devise_token_auth_for 'User', at: 'api/auth'
- 
+
   namespace :api do
     resources :posts do
       resources :answers
