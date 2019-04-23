4903310f187189d3c2b35625ba3c08a783dafa87
Liv Cella <livncella@gmail.com>
2019-04-22 18:55:04 -0600

    pre-pull

--- client/src/components/answers/Answer.js
+++ client/src/components/answers/Answer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Icon, Grid, Divider} from 'semantic-ui-react';
+import {Button, Icon, Grid, Divider, Segment } from 'semantic-ui-react';
 
 const Answer = ({ id, body, post_id, updateAnswer, deleteAnswer }) => (
   <div >
@@ -7,23 +7,25 @@ const Answer = ({ id, body, post_id, updateAnswer, deleteAnswer }) => (
 
       <div>
       <Grid>
-        <Grid.Row>
-          <Grid.Column width={15}>
-            <p>{body}</p>
-          </Grid.Column>
 
-          <Grid.Column>
-            <Button
-              icon
-              inverted color="red"
-              floated="right"
-              size="mini"
-              onClick={() => deleteAnswer(id, post_id)}
-              >
-              <Icon name="trash" />
-              </Button>
-          </Grid.Column>
-          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={15}>
+              <p>{body}</p>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Button
+                icon
+                inverted color="red"
+                floated="right"
+                size="mini"
+                onClick={() => deleteAnswer(id, post_id)}
+                >
+                <Icon name="trash" />
+                </Button>
+            </Grid.Column>
+            </Grid.Row>
+            
           </Grid>
       </div>
 
