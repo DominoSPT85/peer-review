589516fe8d7360393f81f99bb17efaf4547305ca
Liv Cella <livncella@gmail.com>
2019-04-26 14:15:21 -0600

    removed root

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  ROOT_URL=https://peer-review-dpl-spring.herokuapp.com/
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
