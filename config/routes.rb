74d65aa0519c9ebcb3e25165adae10e2df433702
Liv Cella <livncella@gmail.com>
2019-04-26 00:39:47 -0600

    more heroku madness

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => 'index#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount_devise_token_auth_for 'User', at: 'api/auth'
